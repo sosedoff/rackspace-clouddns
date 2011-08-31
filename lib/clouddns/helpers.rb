61c35b76c2af8d5eb78cfb5e8ae2203f31795e7c
Dan Sosedoff <dan.sosedoff@gmail.com>
2011-08-31 09:53:00 -0500

    rework regexes

--- lib/clouddns/helpers.rb
+++ lib/clouddns/helpers.rb
@@ -1,6 +1,8 @@
 module CloudDns
   module Helpers
-    DOMAIN_REGEX = /^[a-z\d-]+(\.[a-z\d-]+)*\.(([\d]{1,3})|([a-z]{2,3})|(aero|coop|info|museum|name))$/i
+    REGEX_DOMAIN = /^[a-z\d-]+(\.[a-z\d-]+)*\.(([\d]{1,3})|([a-z]{2,3})|(aero|coop|info|museum|name))$/i
+    REGEX_IP     = /^[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}$/
+    REGEX_AAAA   = /^([a-f\d]{1,}:?)+/i
     
     # Validate a domain name
     #
