67d6c1f38d4a92f6fd93850bf1725c436ca82816
Dan Sosedoff <dan.sosedoff@gmail.com>
2011-08-31 10:02:02 -0500

    validation helpers

--- lib/clouddns/helpers.rb
+++ lib/clouddns/helpers.rb
@@ -1,13 +1,25 @@
 module CloudDns
   module Helpers
     REGEX_DOMAIN = /^[a-z\d-]+(\.[a-z\d-]+)*\.(([\d]{1,3})|([a-z]{2,3})|(aero|coop|info|museum|name))$/i
-    REGEX_IP     = /^[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}$/
-    REGEX_AAAA   = /^([a-f\d]{1,}:?)+/i
+    REGEX_IPV4   = /^[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}$/
+    REGEX_IPV6   = /^([a-f\d]{1,}:?)+/i
     
     # Validate a domain name
     #
     def validate_domain(value)
-      value =~ DOMAIN_REGEX
+      value =~ REGEX_DOMAIN
+    end
+    
+    # Validate an IP address
+    #
+    def validate_ip(value)
+      value =~ REGEX_IPV4
+    end
+    
+    # Validate an IPv6 address
+    #
+    def validate_ip6(value)
+      value =~ REGEX_IPV6
     end
   end
 end
