0390da69f98da51ab54917c89d14b0e4b7a6745f
Darwin <darwin.git@marianna.se>
2011-10-07 11:41:38 +0200

    made it a bit nicer

--- lib/clouddns/client.rb
+++ lib/clouddns/client.rb
@@ -26,14 +26,16 @@ module CloudDns
     #
     # options[:username] - RackspaceCloud API username
     # options[:api_key]  - RackspaceCloud API key
-    # options[:api_auth] - RackspaceCloud API_AUTH URL, (defaults to US url)
-    # options[:api_base] - RackspaceCloud API URL, (defaults to US url)
+    # options[:location] - RackspaceCloud account location, (default to :us)
     #
     def initialize(options={})
+      
+      options[:location] ||= :us
+      
       @username   = options[:username].to_s
       @api_key    = options[:api_key].to_s
-      @api_auth   = options[:api_auth] || CloudDns::API_AUTH[:us]
-      @api_base   = options[:api_base] || CloudDns::API_BASE[:us]
+      @api_auth   = CloudDns::API_AUTH[options[:location]]
+      @api_base   = CloudDns::API_BASE[options[:location]]
       
       raise ArgumentError, "Client :username required!" if @username.empty?
       raise ArgumentError, "Client :api_key required!"  if @api_key.empty?
